f3da4eb51ba32049c5738460d41c150e6968ad64
Mark  Samuel <mark.girgis.samuel@gmail.com>
2024-02-02 12:36:06 +0200

    updating backup scripts

--- backup/db_backup.sh.sh
+++ backup/db_backup.sh.sh
@@ -6,7 +6,7 @@ DB_PASSWORD=root
 DB_SID=XE
 
 # Absolute Backup Directory
-BACKUP_DIR=~/Desktop/UNIV/backup/logging
+BACKUP_DIR=~/Desktop/UNIV/backup
 
 # Date Format for Backup File
 DATE_FORMAT=$(date +"%Y%m%d_%H%M%S")
@@ -18,10 +18,13 @@ EXPORT_FILE="backup_${DATE_FORMAT}.dmp"
 expdp ${DB_USER}/${DB_PASSWORD}@${DB_SID} DIRECTORY=DATA_PUMP_DIR DUMPFILE=${EXPORT_FILE} FULL=Y
 
 # Check if the export was successful
+# if successful copy back file to your dir
 if [ $? -eq 0 ]; then
-    echo "Database backup successful. | File: ${EXPORT_FILE} | date: `date` " >> ${BACKUP_DIR}/space_logs.txt
-    echo "-------------------------------------------------------------" >> ${BACKUP_DIR}/space_logs.txt
+    cp /c/oraclexe/app/oracle/admin/XE/dpdump/${EXPORT_FILE} ${BACKUP_DIR}/backup_files 
+    echo "Database backup successful. | File: ${EXPORT_FILE} | date: `date` " >> ${BACKUP_DIR}/logging/logs.txt
+    echo "-------------------------------------------------------------" >> ${BACKUP_DIR}/logging/logs.txt
 else
-    echo "Error: Database backup failed. | date: `date` " >> ${BACKUP_DIR}/space_logs.txt
-    echo "-------------------------------------------------------------" >> ${BACKUP_DIR}/space_logs.txt
+    echo "Error: Database backup failed. | date: `date` " >> ${BACKUP_DIR}/logging/logs.txt
+    echo "-------------------------------------------------------------" >> ${BACKUP_DIR}/logging/logs.txt
 fi
+
--- backup/disk_logs.sh.sh
+++ backup/disk_logs.sh.sh
@@ -5,15 +5,15 @@ threshold=40
 # Check disk space usage
 disk_usage=$(df -h / | awk 'NR==2 {print $6}' | tr -d '%' | cut -d'C' -f1)
 free_space=$(df -h / | awk 'NR==2 {print $5}' | tr -d '%' | cut -d'C' -f1)
-echo "disk_usage: $disk_usage% | free_space: $free_space | date: `date`" >> ~/Desktop/UNIV/backup/logging/space_logs.txt
+echo "disk_usage: $disk_usage% | free_space: $free_space | date: `date`" >> ~/Desktop/UNIV/backup/logging/logs.txt
 
 # Compare with the threshold
 if [ "$disk_usage" -ge "$threshold" ]; then
     # Send alert/notification (replace with your notification mechanism)
-    echo "Warning: Disk space usage is above $threshold%. Consider freeing up space." >> ~/Desktop/UNIV/backup/logging/space_logs.txt
-    echo "-------------------------------------------------------------" >> ~/Desktop/UNIV/backup/logging/space_logs.txt
+    echo "Warning: Disk space usage is above $threshold%. Consider freeing up space." >> ~/Desktop/UNIV/backup/logging/logs.txt
+    echo "-------------------------------------------------------------" >> ~/Desktop/UNIV/backup/logging/logs.txt
 else
     echo "Disk space usage is within acceptable limits." >> ~/Desktop/UNIV/backup/logging/space_logs.txt 
-    echo "-------------------------------------------------------------" >> ~/Desktop/UNIV/backup/logging/space_logs.txt
+    echo "-------------------------------------------------------------" >> ~/Desktop/UNIV/backup/logging/logs.txt
 fi
 
